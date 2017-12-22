2ff1a88c24116d1ddb631acef7503b461d637cc2
Xavier Naunay <xavierxc14@gmail.com>
2017-12-22 14:47:10 +0100

    Initial commit

---
+++ src/main/java/com/dsc/carsharing/CarsharingApplication.java
@@ -0,0 +1,12 @@
+package com.dsc.carsharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsharingApplication.class, args);
+	}
+}

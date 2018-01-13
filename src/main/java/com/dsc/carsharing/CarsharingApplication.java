c9bb71acedda239f28b51068cba20d8ef69e1259
kimeur <gazouzimarouane@gmail.com>
2018-01-14 02:48:49 +0300

    "add user Toto/ add tab proposals"

--- src/main/java/com/dsc/carsharing/CarsharingApplication.java
+++ src/main/java/com/dsc/carsharing/CarsharingApplication.java
@@ -1,5 +1,11 @@
 package com.dsc.carsharing;
 
+import com.dsc.carsharing.model.Administrator;
+import com.dsc.carsharing.model.User;
+import com.dsc.carsharing.repositories.AdministratorRepository;
+import com.dsc.carsharing.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +17,14 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import java.util.Locale;
 
 @SpringBootApplication
-public class CarsharingApplication {
+public class CarsharingApplication{
+
+    @Autowired
+    private AdministratorRepository administratorRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public static void main(String[] args) {
         SpringApplication.run(CarsharingApplication.class, args);
@@ -28,4 +41,5 @@ public class CarsharingApplication {
         resolver.setDefaultLocale(Locale.US);
         return resolver;
     }
+
 }
--- src/main/java/com/dsc/carsharing/config/SetupDataLoader.java
+++ src/main/java/com/dsc/carsharing/config/SetupDataLoader.java
@@ -1,7 +1,9 @@
-package templates.error;
+package com.dsc.carsharing.config;
 
 import com.dsc.carsharing.model.Administrator;
+import com.dsc.carsharing.model.User;
 import com.dsc.carsharing.repositories.AdministratorRepository;
+import com.dsc.carsharing.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -17,17 +19,22 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
 
     @Autowired
     private AdministratorRepository administratorRepository;
-
+    @Autowired
+    private UserRepository userRepository;
     @Autowired
     private PasswordEncoder passwordEncoder;
 
     @Override
     @Transactional
     public void onApplicationEvent(final ContextRefreshedEvent event) {
+        System.out.println("okok");
         if (alreadySetup) {
+            System.out.println("vu");
             return;
         }
+        System.out.println("dada");
         createAdmin();
+        createUser();
         alreadySetup = true;
     }
 
@@ -43,6 +50,21 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
             admin.setEnabled(true);
             admin.setPassword(passwordEncoder.encode("administrator12345"));
             administratorRepository.save(admin);
+        }
+    }
+
+    private void createUser() {
+        String username = "toto";
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            user = new User();
+            user.setFirstName("toto");
+            user.setLastName("toto");
+            user.setEmail("toto@carsharing.com");
+            user.setUsername(username);
+            user.setEnabled(true);
+            user.setPassword(passwordEncoder.encode("toto"));
+            userRepository.save(user);
         }
     }
 

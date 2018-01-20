53e8e45f7bd52792c71861d59f44bded17c6169c
Xavier Naunay <xavierxc14@gmail.com>
2018-01-20 22:47:00 +0100

    Adds statistics

--- src/main/java/com/dsc/carsharing/controllers/UserController.java
+++ src/main/java/com/dsc/carsharing/controllers/UserController.java
@@ -1,39 +1,30 @@
 package com.dsc.carsharing.controllers;
 
 
-import com.dsc.carsharing.model.Excursion;
-import com.dsc.carsharing.repositories.UserRepository;
 import com.dsc.carsharing.repositories.ExcursionRepository;
-import com.dsc.carsharing.repositories.GroupRepository;
+import com.dsc.carsharing.repositories.ProposalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/statistics")
 public class UserController {
 
-	@Autowired
-	private UserRepository userRepository;
-	
-	@Autowired
-    private ExcursionRepository excursionRepository;
+    @Autowired
+    private ProposalRepository proposalRepository;
 
     @Autowired
-    private GroupRepository groupRepository;
-    
-    @RequestMapping(path = "", method = RequestMethod.GET)
-    public String indexBis() {
-    	long x = userRepository.count();
-    	long y = excursionRepository.count();
-    	long z = groupRepository.count();
-        return "statistics/tableauDeBord";
+    private ExcursionRepository excursionRepository;
+
+    @GetMapping
+    public String index(Model model) {
+        long proposals = proposalRepository.count();
+        long excursions = excursionRepository.count();
+        model.addAttribute("proposals", proposals);
+        model.addAttribute("excursions", excursions);
+        return "statistics/list";
     }
 }

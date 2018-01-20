5fc50a3943817c9c544c7c8c2e8b632cef5a9967
Nassim TOUATI <xbox_007@hotmail.fr>
2018-01-20 16:17:39 +0100

    Add files via upload

---
+++ src/main/java/com/dsc/carsharing/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.dsc.carsharing.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	@Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()               
+	                .apis(RequestHandlerSelectors.any())
+	                .paths(regex("/api.*"))
+	                .build()
+	                .apiInfo(metaData());
+	    }
+	   
+		public ApiInfo metaData() {
+	        ApiInfo apiInfo = new ApiInfo(
+	                "Car sharing",
+	                "MASTER 2 Données et systèmes connectés",
+	                "1.0",
+	                "Terms of service",
+	                new Contact("Nassim TOUATI - Xavier NAUNAY - Younes el idrissi yazami - Marouan GAZOUZI ", "https://github.com/xavierxc14/car-sharing/blob/master/ProjetMobileDistriDSC.pdf", "nassim.tweti@gmail.com"),
+	               "Apache License Version 2.0",
+	                "https://www.apache.org/licenses/LICENSE-2.0");
+	        return apiInfo;
+	    }
+	   
+}
+
+
+
